0af2c877335f7cf6d04e5d9b42f0706fec95871d
Kiril <irodin007@gmail.com>
2020-02-23 20:33:29 +0300

    rewrite with class

--- Lab_1_Csharp/Program.cs
+++ Lab_1_Csharp/Program.cs
@@ -2,11 +2,17 @@
 
 namespace Lab_1_Csharp
 {
-    class Program
+    class Game
     {
-        //entering the field
-        static int IntInputUser()
+        public int rows;
+        private int columns;
+        private int ships;
+        private int[,] field;
+        private int attempts;
+
+        static int InputDimension()
         {
+            Console.WriteLine("Enter a size of the field");
             int number;
             string get;
             get = Console.ReadLine();
@@ -18,13 +24,80 @@ namespace Lab_1_Csharp
             return number;
         }
 
-        //entering coordinates of the shooting
-        static int ShootCoor(string[,] field)
+        void GenerateField()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    field[i, j] = 0;
+                }
+            }
+
+            Random rand = new Random();
+            int x, y;
+            bool checker;
+            for (int i = 0; i < ships;)
+            {
+                checker = false;
+                int seed = rand.Next(2);
+                switch (seed)
+                {
+                    case 0: //horizantal
+                        y = rand.Next(rows - 2);
+                        x = rand.Next(rows);
+                        for (int k = 0; k < 3; k++)
+                        {
+                            //check for a existing
+                            if (field[x, y] == 1)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                checker = true;
+                            }
+                            field[x, y] = 1;
+                            y++;
+                        }
+                        if (checker)
+                        {
+                            i++;
+                        }
+                        break;
+
+                    case 1: //vertical
+                        x = rand.Next(rows - 2);
+                        y = rand.Next(rows);
+                        for (int k = 0; k < 3; k++)
+                        {
+                            //check for a existing
+                            if (field[x, y] == 1)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                checker = true;
+                            }
+                            field[x, y] = 1;
+                            x++;
+                        }
+                        if (checker)
+                        {
+                            i++;
+                        }
+                        break;
+                }
+            }
+        }
+
+        int InputCoordinate()
         {
             int number;
             string get;
             get = Console.ReadLine();
-            while (!int.TryParse(get, out number) || number > field.GetLength(0) || number < 1)
+            while (!int.TryParse(get, out number) || number > rows || number < 1)
             {
                 Console.WriteLine("Enter a correct coordinates");
                 get = Console.ReadLine();
@@ -32,60 +105,9 @@ namespace Lab_1_Csharp
             return number;
         }
 
-        //creating the field
-        static void CreateField(string[,] field)
-        {
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    field[i, j] = " O";
-                }
-            }
-        }
-
-        
-        static void CreateBackOffield(bool[,] checkField)
-        {
-            for (int i = 0; i < checkField.GetLength(0); i++)
-            {
-                for (int j = 0; j < checkField.GetLength(1); j++)
-                {
-                    checkField[i, j] = false;
-                }
-            }
-        }
-
-        //drawing the field
-        static void Draw(string[,] field)
-        {
-            Console.Write("   ");
-
-            for (int i = 1; i < field.GetLength(0) + 1; i++)
-            {
-                Console.Write(" {0}",i);
-                
-            }
-            Console.WriteLine();
-
-            int k = 1;
-
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                Console.Write("{0, 3}", " " + k);
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    Console.Write(field[i, j]);
-                }
-                Console.WriteLine();
-                k++;
-            }
-        }
-
-        static int Attempts(string[,] field)
+        void Attempts()
         {
             Console.WriteLine("How many attempts do you want?");
-            int attempts;
             string get;
             get = Console.ReadLine();
             while (!int.TryParse(get, out attempts) || attempts > 100 || attempts < 6)
@@ -98,145 +120,108 @@ namespace Lab_1_Csharp
                 attempts = field.Length - 1;
                 Console.WriteLine("Amount of attempts is {0}", field.Length - 1);
             }
+        }
+
+        public Game()
+        {
+            rows = InputDimension();
+            columns = rows;
+            field = new int[rows, columns];
+            ships = 2;
+            GenerateField();
+            Attempts();
+        }
+
+        void Draw()
+        {
+            Console.Write("   ");
+
+            for (int i = 1; i < field.GetLength(0) + 1; i++)
+            {
+                Console.Write(" {0}", i);
+
+            }
+            Console.WriteLine();
+
+            for (int i = 1; i < field.GetLength(0)+1; i++)
+            {
+                Console.Write("{0, 3}", " " + i);
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    switch (field[i - 1, j])
+                    {
+                        case 2:
+                            Console.Write(" X");
+                            break;
+                        case 3:
+                            Console.Write(" .");
+                            break;
+                        default:
+                            Console.Write(" O");
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        int CountLiveShips()
+        {
+            int liveShips = 0;
+            for (int i = 1; i < rows + 1; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (field[i - 1, j] == 1)
+                    {
+                        liveShips++;
+                    }
+                }
+            }
+            return liveShips;
+        }
+
+        public int Shoot()
+        {
+            Draw();
+            int xShoot = InputCoordinate();
+            int yShoot = InputCoordinate();
+            Console.Clear();
+            attempts--;
+
+            switch (field[xShoot - 1, yShoot - 1])
+            {
+                case 0:
+                    field[xShoot - 1, yShoot - 1] = 3;
+                    Console.WriteLine("You missed");
+                    break;
+                case 1:
+                    field[xShoot - 1, yShoot - 1] = 2;
+                    Console.WriteLine("Right on target!");
+                    break;
+            }
+
+            if (CountLiveShips() == 0)
+            {
+                Console.WriteLine("Well played!!!");
+                Console.ReadLine();
+                return -1;
+            }
+
             return attempts;
         }
+    }
 
-        static void RandomGenShips(bool[,] checkField, int amountOfShips)
-        {
-            Random rand = new Random();
-            int i, j;
-            bool checker;
-            for (int l = 0; l < amountOfShips; )
-            {
-                checker = false;
-                int seed = rand.Next(2);
-                switch (seed)
-                {
-                    case 0: //gorizantal
-                        j = rand.Next(checkField.GetLength(0) - 2);
-                        i = rand.Next(checkField.GetLength(0));
-                        for (int k = 0; k < 3; k++)
-                        {
-                            //check for a existing
-                            if (checkField[i, j])
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                checker = true;
-                            }
-                            checkField[i, j] = true;
-                            j++;
-                        }
-                        if (checker)
-                        {
-                            l++;
-                        }
-                        break;
-
-                    case 1: //vertical
-                        i = rand.Next(checkField.GetLength(0) - 2);
-                        j = rand.Next(checkField.GetLength(0));
-                        for (int k = 0; k < 3; k++)
-                        {
-                            //check for a existing
-                            if (checkField[i, j])
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                checker = true;
-                            }
-                            checkField[i, j] = true;
-                            i++;
-                        }
-                        if (checker)
-                        {
-                            l++;
-                        }
-                        break;
-                }
-            }
-        }
-
-
-
-        //game procces
-        static void GameLogic(string[,] field, bool[,] checkField, int attempts, int amountOfShips)
-        {
-            int counter = 0;
-            int winCounter = 0;
-            bool inGame = true;
-            while (inGame)
-            {
-                Draw(field);
-                Console.WriteLine("Take your shoot");
-                int xShoot = ShootCoor(field);
-                int yShoot = ShootCoor(field);
-                Console.Clear();
-
-                //shooting
-                if (checkField[xShoot - 1, yShoot - 1])
-                {
-                    field[xShoot - 1, yShoot - 1] = " X";
-                    Console.WriteLine("Right on target!");
-                    winCounter++;
-                }
-                else
-                {
-                    field[xShoot - 1, yShoot - 1] = " .";
-                    Console.WriteLine("You missed");
-                }
-                //checking of the end
-                if (counter == attempts)
-                {
-                    Console.WriteLine("Don`t worry");
-                    inGame = false;
-                    Console.ReadLine();
-                }
-                if (winCounter == amountOfShips * 3)
-                {
-                    Console.WriteLine("Well played!!!");
-                    inGame = false;
-                    Console.ReadLine();
-                }
-                counter++;
-            }
-        }
-
+    class Program
+    {
         static void Main()
         {
-            int amountOfShips = 2;
-
-            Console.WriteLine("Enter field size, please");
-            int sizeField = IntInputUser();
-
-
-            //creating the field array
-            string[,] field = new string[sizeField, sizeField];
-
-            CreateField(field);
-
-
-            //creating the back of field array
-            bool[,] checkField = new bool[sizeField, sizeField];
-
-            CreateBackOffield(checkField);
-
-
-            //entering a number of shoots
-            int attempts = Attempts(field);
-
-            //spawning ships
-            RandomGenShips(checkField, amountOfShips);
-
-
-            Console.WriteLine("To play, enter two numbers");
-
-            //start
-            GameLogic(field, checkField, attempts, amountOfShips);
+            Game game= new Game();
+            do
+            {
+                Console.WriteLine("Take your shoot");
+            }
+            while (game.Shoot() > 0);
             Console.WriteLine("Thanks for a game");
             Console.WriteLine("See you next time");
         }
